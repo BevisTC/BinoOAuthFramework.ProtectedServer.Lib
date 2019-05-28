b79ff115d3a7d11b70c59a98f1a9b3e15f442b41
DESKTOP-TA4PFCR\allen <allenwang@binodata.com>
2019-05-28 17:26:44 +0800

    ADd

---
+++ BinoOAuthFramework.ProtectedServer.Lib/BinoOAuthFramework.ProtectedServer.Lib/OAuthNoticeVerifier.cs
@@ -0,0 +1,13 @@
+﻿using BinoOAuthFramework.ProtectedServer.Lib.Entities;
+using System;
+
+namespace BinoOAuthFramework.ProtectedServer.Lib
+{
+    public class OAuthNoticeVerifier
+    {
+        public OAuthNoticeVerifier(ProtectedServerModel server)
+        {
+
+        }
+    }
+}
---
+++ BinoOAuthFramework.ProtectedServer.Lib/BinoOAuthFramework.ProtectedServer.Lib/OAuthRegister/Core/Authz/IResourceAuthZService.cs
@@ -0,0 +1,12 @@
+﻿using BinoOAuthFramework.ProtectedServer.Lib.OAuthRegister.Model.Authz.ResrcProtector;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinoOAuthFramework.ProtectedServer.Lib.OAuthRegister.Core.Authz
+{
+    public interface IResourceAuthZService
+    {
+        //Optional<string> AuthorizeClient(ApiAuthorizeModel apiAuthZModel);
+    }
+}
